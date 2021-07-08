51770bbe6c730b7a6b9a3bb8633272865bf246c6
Beicker Zambrano <beicker998@gmail.com>
2021-07-08 18:04:30 -0500

    Add more film lists components
    Add more lists to show all the important data and adapt lists styles to
    mobile with fewer items to place them without problem

--- components/FilmList/index.js
+++ components/FilmList/index.js
@@ -1,17 +1,34 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import FilmCard from "../FilmCard";
 import styles from "./styles.module.css";
 
 export default function FilmList({ films, title }) {
+  const [dimensions, setDimensions] = useState({
+    width: 0,
+  });
+  useEffect(() => {
+    setDimensions({
+      width: window.innerWidth,
+    });
+  }, []);
+
+  const mapFilms = (films, limit) =>
+    films.slice(0, limit).map((film, index) => {
+      return (
+        <Fragment key={index}>
+          <FilmCard src={process.env.API_IMAGE_URL + film.poster_path} />
+        </Fragment>
+      );
+    });
+
   return (
-    <div className={styles.list}>
-      {films.slice(0, 7).map((film, index) => {
-        return (
-          <Fragment key={index}>
-            <FilmCard src={process.env.API_IMAGE_URL + film.poster_path} />
-          </Fragment>
-        );
-      })}
-    </div>
+    <Fragment>
+      <div className={styles["list-title"]}>
+        <h5>{title}</h5>
+      </div>
+      <div className={styles.list}>
+        {dimensions.width > 1024 ? mapFilms(films, 7) : mapFilms(films, 3)}
+      </div>
+    </Fragment>
   );
 }
